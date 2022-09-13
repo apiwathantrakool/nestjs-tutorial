27434bbbccfaef4e6c1e84bd9dd67889f70e1da7
nekkytechcombine <nekky@techcombine.co>
2022-09-13 16:14:07 +0700

    [Section02]: Management api - get by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,21 @@
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 
+// For example, http://localhost:3000/tasks
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksServe: TasksService) {}
 
-  @Get() // For example, http://localhost:3000/tasks
+  @Get()
   getAllTasks(): Task[] {
     return this.tasksServe.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksServe.getTaskById(id);
   }
 
   @Post()
