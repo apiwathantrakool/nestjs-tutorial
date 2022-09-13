337782d375ea5b8d08424a04f8c31c6096bef75c
nekkytechcombine <nekky@techcombine.co>
2022-09-13 15:58:33 +0700

    [Section02]: Management api - Basic post api

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksServe: TasksService) {}
 
   @Get() // For example, http://localhost:3000/tasks
-  getAllTasks() {
+  getAllTasks(): Task[] {
     return this.tasksServe.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body('title') title, @Body('description') description): Task {
+    return this.tasksServe.createTask(title, description);
   }
 }
