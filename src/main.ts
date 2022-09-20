33a89c8d0f40b517d548cc05b4c046a8c6ddba25
nekkytechcombine <nekky@techcombine.co>
2022-09-20 22:33:14 +0700

    [section-cors]: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: ['http://localhost:3001'] });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = 3000;
