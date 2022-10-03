65d47527b35882bf2d6a68f9e766a908d68b1013
nekkytechcombine <nekky@techcombine.co>
2022-10-03 15:38:47 +0700

    refactor: reset files for new module section

--- src/tasks/dto/update-task-status.dto.ts
+++
@@ -1,9 +0,0 @@
-import { TaskStatus } from '../task.model';
-import { IsEnum } from 'class-validator';
-
-export class UpdateTaskStatusDto {
-  @IsEnum(TaskStatus, {
-    message: `Should be one of [${TaskStatus.DONE}, ${TaskStatus.IN_PROGRESS}, ${TaskStatus.OPEN}]`,
-  })
-  status: TaskStatus;
-}
